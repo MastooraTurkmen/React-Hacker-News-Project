e6227b365fae1f659d41530490c9e8d080750523
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-17 09:51:41 +0430

    set stories

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,14 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true }
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        page: action.payload.page,
+        nbPages: action.payload.nbPages,
+      }
     default:
       throw new Error(`no matching "${action.type}" action type`)
   }
