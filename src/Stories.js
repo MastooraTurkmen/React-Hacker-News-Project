e200802e6d9158aa59252ddcb2e56b055507abda
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-17 09:54:39 +0430

    set up loading

--- src/Stories.js
+++ src/Stories.js
@@ -3,6 +3,12 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
+  const { isLoading } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className='loading'></div>
+  }
+
   return <h2>stories component</h2>
 }
 
