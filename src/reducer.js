3069280c6a982f021d70fe34368931bcf3ffb854
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-17 08:25:50 +0430

    use of Switch

--- src/reducer.js
+++ src/reducer.js
@@ -6,5 +6,12 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true }
+    default:
+      throw new Error(`no matching "${action.type}" action type`)
+  }
+}
 export default reducer
