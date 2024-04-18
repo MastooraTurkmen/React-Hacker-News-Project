3097679103566f28ee196b7ea00b0ceed9573777
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-18 09:04:44 +0430

    set up hacker news

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -5,12 +5,13 @@ const SearchForm = () => {
   const { query, handleSearch } = useGlobalContext()
 
   return (
-    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
-      <h2>seach hacker news</h2>
-      <input type="text"
-        className="form-input"
+    <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+      <h2>search hacker news</h2>
+      <input
+        type='text'
+        className='form-input'
         value={query}
-        onClick={(e) => handleSearch(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
       />
     </form>
   )
