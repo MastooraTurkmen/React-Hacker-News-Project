699a20ca5a65be45e6814f6d8f663c107dae1b94
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-18 09:04:47 +0430

    set up hacker news

--- src/Stories.js
+++ src/Stories.js
@@ -3,27 +3,40 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  const { isLoading, hits, removeStory } = useGlobalContext();
+  const { isLoading, hits, removeStory } = useGlobalContext()
 
   if (isLoading) {
     return <div className='loading'></div>
   }
-
   return (
-    <section className="stories">
+    <section className='stories'>
       {hits.map((story) => {
-        const { objectID, num_comments, title, points, url, author } = story
-        return <article key={objectID} className="story">
-          <h4 className="title">{title}</h4>
-          <p className="info">
-            {points} points by <span>{author} | </span>
-            {num_comments}{''} comments
-          </p>
-          <div>
-            <a target='_blank' rel="noreferrer" href={url} className='read-link'>read more</a>
-            <button onClick={() => removeStory(objectID)} className="remove-btn">remove</button>
-          </div>
-        </article>
+        const { objectID, title, num_comments, url, points, author } = story
+        return (
+          <article key={objectID} className='story'>
+            <h4 className='title'>{title}</h4>
+            <p className='info'>
+              {points} points by <span>{author} | </span> {num_comments}{' '}
+              comments
+            </p>
+            <div>
+              <a
+                href={url}
+                className='read-link'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                read more
+              </a>
+              <button
+                className='remove-btn'
+                onClick={() => removeStory(objectID)}
+              >
+                remove
+              </button>
+            </div>
+          </article>
+        )
       })}
     </section>
   )
